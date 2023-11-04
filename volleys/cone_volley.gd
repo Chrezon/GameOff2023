22f6390aa925ad82b6d94f4de52bae1618ddfcdf
huhinchang <huhinchang@gmail.com>
2023-11-04 13:23:48 -0400

    parallel volley

--- volleys/cone_volley.gd
+++ volleys/cone_volley.gd
@@ -3,7 +3,7 @@ extends Volley
 class_name ConeVolley
 
 @export_range(0, 360) var arc : float
-@export var random_in_arc : bool
+@export var randomize_dir : bool
 
 func shoot(origin: Vector2, direction: Vector2, root: Node2D):
 	if num_bullets == 1:
@@ -11,17 +11,17 @@ func shoot(origin: Vector2, direction: Vector2, root: Node2D):
 		return
 	
 	var angle_step = deg_to_rad(arc) / (num_bullets - 1)
-	var angle_start = direction.angle() - deg_to_rad(arc) / 2
-	var angle_end = direction.angle() + deg_to_rad(arc) / 2
+	var angle_min = direction.angle() - deg_to_rad(arc) / 2
+	var angle_max = direction.angle() + deg_to_rad(arc) / 2
 	
-	if random_in_arc:
+	if randomize_dir:
 		for i in range(num_bullets):
 			_shoot_single(origin,
-				Vector2.RIGHT.rotated(randf_range(angle_start, angle_end)),
+				Vector2.RIGHT.rotated(randf_range(angle_min, angle_max)),
 				root)
 	else:
 		for i in range(num_bullets):
 			_shoot_single(origin,
-				Vector2.RIGHT.rotated(angle_start + angle_step * i),
+				Vector2.RIGHT.rotated(angle_min + angle_step * i),
 				root)
 
---
+++ volleys/parallel_volley.gd
@@ -0,0 +1,23 @@
+extends Volley
+
+class_name ParallelVolley
+
+@export_range(0, 1000) var width : float
+@export var randomize_pos : bool
+
+func shoot(origin: Vector2, direction: Vector2, root: Node2D):
+	var pos_step = width / (num_bullets - 1)
+	var pos_min = origin.x - (width/2)
+	var pos_max = origin.x + (width/2)
+
+	if randomize_pos:
+		for i in range(num_bullets):
+			_shoot_single(Vector2(randf_range(pos_min, pos_max), origin.y),
+				direction,
+				root)
+	else:
+		for i in range(num_bullets):
+			_shoot_single(Vector2(pos_min + pos_step * i, origin.y),
+				direction,
+				root)
+
