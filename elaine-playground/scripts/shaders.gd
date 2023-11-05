b0dc24552c94332e9693e55a86b54b5a10376896
Chrezon <40221885+Chrezon@users.noreply.github.com>
2023-11-05 14:04:52 -0500

    added button to test shader anims

--- elaine-playground/scripts/shaders.gd
+++ elaine-playground/scripts/shaders.gd
@@ -1,12 +1,19 @@
 extends Control
 
+var animationPlayer
+var shader
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	shader = get_node("TextureRect")
+	animationPlayer = get_node("TextureRect/AnimationPlayer")
+	shader.visible = false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+func _on_player_slow_time_begin():
+	shader.visible = true
+	animationPlayer.play("Anim")
+	
+func _on_player_slow_time_end():
+	shader.visible = false
+	animationPlayer.play("RESET")
+	
---
+++ elaine-playground/scripts/test-player.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+# TODO: maybe even has params
+signal slowTimeBegin
+signal slowTimeEnd
+
+# This is basically just emulating an actual
+# player class.
+func _on_button_pressed():
+	slowTimeBegin.emit()
+	await get_tree().create_timer(2.0).timeout
+	slowTimeEnd.emit()
