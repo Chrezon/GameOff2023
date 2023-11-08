14cf69907fc22db3416da3685ddbdcedd5211d9a
huhinchang <huhinchang@gmail.com>
2023-11-07 22:46:06 -0500

    Made spinny bullet inherit from laser bullet
    we only need to change the laser bullet for the changes to propagate to the spinny bullet

--- volleys/bullet_volley.gd
+++ volleys/bullet_volley.gd
@@ -3,7 +3,7 @@ extends Volley
 class_name BulletVolley
 
 @export_range(1, 100) var num_bullets : int
-@export var bullet_scene : PackedScene = preload("res://scenes/bullet2.tscn")
+@export var bullet_scene : PackedScene = preload("res://scenes/laser_bullet.tscn")
 
 func shoot(origin: Vector2, direction: Vector2, root: Node2D):
 	pass
