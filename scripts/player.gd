42197bd5d804724dffe939835415f6c40427140e
huhinchang <huhinchang@gmail.com>
2023-11-11 16:59:26 -0500

    fixed player moving faster diagonally

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@export var SPEED = 300.0
+@export var MAX_SPEED = 300.0
+@export var ACCELERATION = 10000.0
 
 signal hit
 
@@ -12,22 +13,15 @@ var gravity = 0 #ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	velocity.y = -SPEED
+	var targetVel: Vector2
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var horizontal_dir = Input.get_axis("move_left", "move_right")
-	if horizontal_dir:
-		velocity.x = horizontal_dir * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	var vertical_dir = Input.get_axis("move_up", "move_down")
-	if vertical_dir:
-		velocity.y = vertical_dir * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+	targetVel.x = Input.get_axis("move_left", "move_right")
+	targetVel.y = Input.get_axis("move_up", "move_down")
+	targetVel = targetVel.normalized() * MAX_SPEED
+	
+	velocity = velocity.move_toward(targetVel, ACCELERATION * delta)
 	move_and_slide()
 	
 	if Input.is_action_pressed("slow_down"):
