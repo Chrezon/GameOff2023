735df680e6360c76de886cf42e1c419a7636bfca
huhinchang <huhinchang@gmail.com>
2023-11-11 17:42:18 -0500

    bullets can now aim at the player

--- scripts/bullet_spawner.gd
+++ scripts/bullet_spawner.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 @export var volley_chain : VolleyChain
 
+@onready var player_scene = $"../Player"
+
 var fire_cooldown_remaining = 0
 
 func _process(delta):	
 	fire_cooldown_remaining -= delta * globals.time_scale
-		
+	
 	if (fire_cooldown_remaining <= 0):
 		fire_cooldown_remaining =\
-			volley_chain.shoot_next(self.position, Vector2.DOWN, get_parent())
+			volley_chain.shoot_next(self.position, player_scene.position, get_parent())
