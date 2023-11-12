aa90ab045192c6732f36519478f3f1f62e9343fa
huhinchang <huhinchang@gmail.com>
2023-11-11 19:17:41 -0500

    minor optimization for audio
    caching the master audio bus index

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -7,7 +7,7 @@ var time_scale = 1.0
 @onready var viewport_size = Vector2(
 	ProjectSettings.get_setting("display/window/size/viewport_width"),
 	ProjectSettings.get_setting("display/window/size/viewport_height"))
-
+@onready var master_audio_bus_index = AudioServer.get_bus_index("Master")
 @export var MIN_TIME_SCALE = 0.1
 
 # TODO gradually slow down time instead of snapping
@@ -15,7 +15,7 @@ func _process(delta):
 	var change_speed = 	Input.is_action_pressed("change_speed")
 	time_scale = MIN_TIME_SCALE if change_speed else 1
 
-	for i in AudioServer.get_bus_effect_count(AudioServer.get_bus_index("Master")):
-		AudioServer.set_bus_effect_enabled(AudioServer.get_bus_index("Master"), i, change_speed)
+	for i in AudioServer.get_bus_effect_count(master_audio_bus_index):
+		AudioServer.set_bus_effect_enabled(master_audio_bus_index, i, change_speed)
 
 	change_time.emit()
