4dc051f6603e97ea5bc11428b196b8a7df16e56f
Chrezon <40221885+Chrezon@users.noreply.github.com>
2023-11-11 19:32:28 -0500

    Update globals.gd
    Use button down and button up instead of pressed. Changed emitter to not emit every frame

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,6 +1,7 @@
 extends Node
 
-signal change_time()
+signal change_time_begin()
+signal change_time_end()
 
 var time_scale = 1.0
 
@@ -12,10 +13,15 @@ var time_scale = 1.0
 
 # TODO gradually slow down time instead of snapping
 func _process(delta):
-	var change_speed = 	Input.is_action_pressed("change_speed")
-	time_scale = MIN_TIME_SCALE if change_speed else 1
+	# var change_speed = 	Input.is_action_pressed("change_speed")
+	# time_scale = MIN_TIME_SCALE if change_speed else 1
+	if (Input.is_action_just_pressed("change_speed")):
+		time_scale = MIN_TIME_SCALE
+		change_time_begin.emit()
+	if (Input.is_action_just_released("change_speed")):
+		time_scale = 1
+		change_time_end.emit()
 
 	for i in AudioServer.get_bus_effect_count(master_audio_bus_index):
-		AudioServer.set_bus_effect_enabled(master_audio_bus_index, i, change_speed)
+		AudioServer.set_bus_effect_enabled(master_audio_bus_index, i, time_scale == MIN_TIME_SCALE)
 
-	change_time.emit()
