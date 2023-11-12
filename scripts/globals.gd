9c3104b3c8bc4623827baf91d625ad3a7d835ff6
huhinchang <huhinchang@gmail.com>
2023-11-11 19:04:01 -0500

    music ducks when time is slowed

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -11,10 +11,11 @@ var time_scale = 1.0
 @export var MIN_TIME_SCALE = 0.1
 
 # TODO gradually slow down time instead of snapping
-func _process(delta):	
-	if Input.is_action_pressed("change_speed"):
-		time_scale = MIN_TIME_SCALE
-	else:
-		time_scale = 1
+func _process(delta):
+	var change_speed = 	Input.is_action_pressed("change_speed")
+	time_scale = MIN_TIME_SCALE if change_speed else 1
+
+	for i in AudioServer.get_bus_effect_count(AudioServer.get_bus_index("Master")):
+		AudioServer.set_bus_effect_enabled(AudioServer.get_bus_index("Master"), i, change_speed)
 
 	change_time.emit()
