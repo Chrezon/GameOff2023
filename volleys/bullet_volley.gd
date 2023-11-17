b5f39fa656b3f8fff3bbfc4c6b56e702bcd2526d
huhinchang <huhinchang@gmail.com>
2023-11-16 21:39:42 -0500

    bullet volleys can now have an angular offset

--- volleys/bullet_volley.gd
+++ volleys/bullet_volley.gd
@@ -2,8 +2,9 @@ extends Volley
 
 class_name BulletVolley
 
-@export_range(1, 100) var num_bullets : int
+@export_range(1, 100) var num_bullets : int = 1
 @export_range(0, 1) var left_right : float = 0.5
+@export_range(-180, 180) var _angle_offset : float = 0
 @export var bullet_scene : PackedScene = preload("res://scenes/laser_bullet.tscn")
 
 func shoot(direction: Vector2, root: Node2D):
@@ -14,6 +15,6 @@ func get_origin() -> Vector2:
 	
 func _shoot_single(spawn_position: Vector2, direction: Vector2, root: Node2D):
 	var b = bullet_scene.instantiate()
-	b.rotation = direction.angle()
+	b.rotation = direction.angle() + deg_to_rad(_angle_offset) 
 	b.position = spawn_position
 	root.add_child(b)
