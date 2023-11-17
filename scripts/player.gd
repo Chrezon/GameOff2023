af12f6cf8803697d5409598d8a08cc8d5d50dc94
huhinchang <huhinchang@gmail.com>
2023-11-16 21:27:21 -0500

    Player also slows down a little when time changes

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,18 @@
 extends CharacterBody2D
 
-@export var MAX_SPEED = 300.0
+@export var MAX_SPEED_NORMAL = 300.0
+@export var MAX_SPEED_SLOW = 200.0
 @export var ACCELERATION = 10000.0
 
 signal hit
 
 @onready var sprite_size = $Sprite2D.get_rect().size * $Sprite2D.scale
+@onready var current_max_speed = MAX_SPEED_NORMAL
+
+func _ready():
+	globals.change_time_begin.connect(func(): current_max_speed = MAX_SPEED_SLOW )
+	globals.change_time_end.connect(func(): current_max_speed = MAX_SPEED_NORMAL )
+	pass
 
 func _physics_process(delta):
 	var targetVel = Vector2.ZERO
@@ -14,7 +21,7 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	targetVel.x = Input.get_axis("move_left", "move_right")
 	targetVel.y = Input.get_axis("move_up", "move_down")
-	targetVel = targetVel.normalized() * MAX_SPEED
+	targetVel = targetVel.normalized() * current_max_speed
 	
 	velocity = velocity.move_toward(targetVel, ACCELERATION * delta)
 	move_and_slide()
